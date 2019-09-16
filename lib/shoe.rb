57381dc0621b752d7be7c8a302d5ebe8252bbdb7
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-16 21:14:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,21 @@
 class Shoe 
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :brand, :color, :size, :material
   
   def initialize(brand)
     @brand = brand
   end 
   
-  def cobble
-    puts "The shoe has been repaired."
+  def condition(condition)
+    @condition = condition
+  end 
+    
+  def condition
+    @condition
   end 
   
-  def condition
-    puts "The shoe is new."
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
   end 
   
 end 
